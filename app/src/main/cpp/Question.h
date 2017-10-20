e6c8fcf2455c7a3ca6801dc82a819727c5e79f82
Yuriy Gromyk <juriygromyk@gmail.com>
2017-10-20 18:09:21 +0300

    Initial commit

---
+++ app/src/main/cpp/Question.h
@@ -0,0 +1,14 @@
+//
+// Created by Gromyk Juriy on 10/20/2017.
+//
+
+#ifndef COURSEWORK_V0_2_QUESTION_H
+#define COURSEWORK_V0_2_QUESTION_H
+
+
+class Question {
+
+};
+
+
+#endif //COURSEWORK_V0_2_QUESTION_H
